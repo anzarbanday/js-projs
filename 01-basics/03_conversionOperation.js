768d38957b2cb29a62284d9060ccf06d940f188c
Anzar Banday <anzar.banday@gameskraft.com>
2023-10-06 22:53:06 +0530

    data type conversions (type casting)

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33"
+
+// type casting
+
+// console.log(typeof(score));
+
+let scoreInInt = Number(score)     // data type starts from a capital letter
+// console.log(typeof(scoreInInt))
+// console.log(scoreInInt)
+
+
+let isLoggedIn = 0
+let boolIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolIsLoggedIn)
+
+
+// 1 => true 0 => false
+// "" => false
+// "string" => true
