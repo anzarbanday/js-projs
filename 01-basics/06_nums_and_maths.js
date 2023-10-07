421b420bc8b99647c36416b6a057cb87ef027aa3
Anzar Banday <anzar.banday@gameskraft.com>
2023-10-07 19:20:20 +0530

    Math Function

---
+++ 01-basics/06_nums_and_maths.js
@@ -0,0 +1,46 @@
+// const score = 400
+// console.log(score)
+
+// const balance = new Number(400.3492349827)
+// console.log(balance)
+
+
+// console.log(balance.toString())
+
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 232.578
+// console.log(otherNumber.toPrecision(3))
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+//************************************************ MATHS  ******************************************/
+
+
+// console.log(Math)
+// console.log(Math.abs(-12))
+
+// console.log(Math.round(4.4))
+
+// console.log(Math.ceil(4.01))
+
+// console.log(Math.floor(4.99))
+
+// console.log(Math.min(4,233,6,556,23,234))
+
+// console.log(Math.max(4,233,6,556,23,234))
+
+
+console.log((Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
