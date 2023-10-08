be35cfc1df8d93720fa5b12a92f4308ded557b35
Anzar Banday <anzar.banday@gameskraft.com>
2023-10-08 12:06:31 +0530

    about dates in javascript

---
+++ 01-basics/07_datesJs.js
@@ -0,0 +1,38 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+
+// console.log(typeof myDate)          //object
+
+// let myCreatedDate = new Date(2023, 9, 7, 5, 6)                    //month starts from 0
+// console.log(myCreatedDate.toLocaleString())
+
+let newCreatedDate = new Date("2023-10-08")
+// console.log(newCreatedDate.toLocaleString())
+
+
+
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+
+// console.log(newCreatedDate.getTime())
+// console.log(Math.floor(Date.now()/1000))
+
+
+let newDate = new Date()
+console.log(newDate.getDay())
+
+console.log(newDate.getMonth()+ 1)
+
+
+
+
+
+
+
